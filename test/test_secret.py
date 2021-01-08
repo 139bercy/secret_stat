10c205c05913b3e94f201ae0c9e740d748749d9a
kaa-serpent <guillaumedecadoudal@ygmail.com>
2021-01-08 15:21:20 +0100

    test

--- test/test_secret.py
+++ test/test_secret.py
@@ -1,8 +1,10 @@
 import json
+from unittest import TestCase
 
+import numpy as np
 import pytest
 import pandas as pd
-from pandas._testing import assert_frame_equal
+from pandas._testing import assert_frame_equal, assert_series_equal
 from main import apply_secret_stat
 
 
@@ -14,17 +16,29 @@ def test_secret_agg():
             "argent1": [0, 0, 0, 0, 0, 999999],
             "argent2": [10, 10, 10, 10, 10, 11]
             }
-    expected = {
+    expected = {  # REGION, TYPE_ENTREPRISE
                 'REGION': [1, 4, 4],
                 'TYPE_ENTREPRISE': ["PME", "GE", "PME"],
-                'argent1': ["NaT", "NaT", "NaT"],
-                'argent2': ["NaT", "NaT", "NaT"]
+                'argent1_count': [np.nan, np.nan, np.nan],
+                'argent1_max': [np.nan, np.nan, np.nan],
+                'argent1_sum': [np.nan, np.nan, np.nan],
+                'argent1_max_percentage': [np.nan, np.nan, np.nan],
+                'argent2_count': [np.nan, np.nan, 4.0],
+                'argent2_max': [np.nan, np.nan, 11.0],
+                'argent2_sum': [np.nan, np.nan, 41.0],
+                'argent2_max_percentage': [np.nan, np.nan, 26.83]
                }
-    expected2 = {
-        'REGION': [1, 4, 4],
-        'TYPE_ENTREPRISE': ["PME", "GE", "PME"],
-        'argent1': ["NaT", "NaT", "NaT"],
-        'argent2': ["NaT", "NaT", "NaT"]
+    expected2 = {  # REGION, ENTREPRISE
+                'REGION': [1, 4, 4],
+                'ENTREPRISE': ["E", "E", "ME"],
+                'argent1_count': [np.nan, np.nan, np.nan],
+                'argent1_max': [np.nan, np.nan, np.nan],
+                'argent1_sum': [np.nan, np.nan, np.nan],
+                'argent1_max_percentage': [np.nan, np.nan, np.nan],
+                'argent2_count': [np.nan, 4.0, np.nan],
+                'argent2_max': [np.nan, 10.0, np.nan],
+                'argent2_sum': [np.nan, 40.0, np.nan],
+                'argent2_max_percentage': [np.nan, 25.0, np.nan]
     }
     expected = pd.DataFrame(expected)
     expected2 = pd.DataFrame(expected2)
@@ -37,20 +51,18 @@ def test_secret_agg():
     test = pd.DataFrame(test)
     group = [
                 ("REGION", "TYPE_ENTREPRISE"),
-                ("ENTREPRISE", "REGION")
+                ("REGION", "ENTREPRISE")
             ]
     col_secret = ("argent1", "argent2")
-    df = apply_secret_stat(group_by=group,
+    df_dict = apply_secret_stat(group_by=group,
                            columns_apply_secret=col_secret,
                            column_to_check="REGION",
                            export_to_csv=True,
                            dataframe=test)
-
-    dictA_str = json.dumps(str(df), sort_keys=True)
-    dictB_str = json.dumps(str(exp3), sort_keys=True)
-    assert dictA_str == dictB_str
-
-    #assert_frame_equal(df, df, check_names=True)
+    for data_frame in df_dict:
+        print(data_frame)
+        assert_frame_equal(df_dict[data_frame], exp3[data_frame])
+    # assert_series_equal(df, exp3)
 
 if __name__ == "__main__":
     test_secret_agg()
